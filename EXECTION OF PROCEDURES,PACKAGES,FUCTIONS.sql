abe42d6e005c454098bef431650e0b084741c5a8
Haneef Muhammad <52352973+haneefmhmmd@users.noreply.github.com>
2023-04-08 14:17:36 -0400

    Update EXECTION OF PROCEDURES,PACKAGES,FUCTIONS.sql

--- EXECTION OF PROCEDURES,PACKAGES,FUCTIONS.sql
+++ EXECTION OF PROCEDURES,PACKAGES,FUCTIONS.sql
@@ -1,3 +1,23 @@
+-- Testing the functions and procedures assosciated with calculating the subtotal, shipping cost, and totalcost
+-- Procedures: CALCULATE_SUBTOTAL, CALCULATE_SHIP_COST
+-- Functions: CALCULATE_TOTAL_COST
+-- The procedure CALCULATE_SUBTOTAL in turn uses the function PRODUCT_PRICE.
+DECLARE
+    lv_ship_cost NUMBER;
+    lv_subtotal INVENTORY.PRICE%TYPE;
+    lv_total_cost NUMBER;
+BEGIN
+    CALCULATE_SUBTOTAL(10002,lv_subtotal);
+    CALCULATE_SHIP_COST('scarborough',lv_ship_cost);
+    DBMS_OUTPUT.PUT_LINE('SubTotal for the order 10002: ' ||lv_subtotal);
+    DBMS_OUTPUT.PUT_LINE('Shipping cost for the location: $' ||lv_ship_cost);
+    
+    lv_total_cost:= CALCULATE_TOTAL_COST(10002,'scarborough');
+    DBMS_OUTPUT.PUT_LINE('Total cost for the order 10002: $' ||lv_total_cost);
+END;
+
+
+
 DECLARE
     lv_customer_id CUSTOMERS.CUSTOMER_ID:= 20231;
     lv_updated_name CUSTOMERS.EMAIL:= 'mackhensley@gmail.com';
