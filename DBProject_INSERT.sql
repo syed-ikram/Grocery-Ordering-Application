f51c98a0409a9906d0ed1d7dd69ddf89085b272d
deannesl08 <128556894+deannesl08@users.noreply.github.com>
2023-04-08 00:47:39 -0400

    Create DBProject_INSERT.sql

---
+++ DBProject_INSERT.sql
@@ -0,0 +1,34 @@
+--INSERT INTO Addresses (address_id,customer_id,address_name,street,city,province,postal_code) VALUES ('100','20231','Home','12 Eglinton Avenue','Toronto','ON','MD1 5H6');
+INSERT INTO Addresses (address_id,customer_id,address_name,street,city,province,postal_code) VALUES (101,20231, 'Office', '451 Lawrence Avenue', 'Toronto', 'ON', 'M4C 6L0');
+INSERT INTO Addresses (address_id,customer_id,address_name,street,city,province,postal_code) VALUES (102,20232, 'Home', '411 Ellesmere Rd', 'Scarborough', 'ON', 'M1G 5K3');
+INSERT INTO Addresses (address_id,customer_id,address_name,street,city,province,postal_code) VALUES (103,20232, 'Office', '101 York Mills Rd', 'Toronto', 'ON', 'M6X 1F5');
+INSERT INTO Addresses (address_id,customer_id,address_name,street,city,province,postal_code) VALUES (104,20233, 'Home', '159 Glenforest Rd', 'Toronto', 'ON', 'M5X 4A7');
+INSERT INTO Addresses (address_id,customer_id,address_name,street,city,province,postal_code) VALUES (105,20234, 'Home', '201 Markham Rd', 'Scarborough', 'ON', 'M9H 4H7');
+INSERT INTO Addresses (address_id,customer_id,address_name,street,city,province,postal_code) VALUES (106,20235, 'Home', '61 Pachino Blvd', 'Scarborough', 'ON', 'M1R 4J6');
+INSERT INTO Addresses (address_id,customer_id,address_name,street,city,province,postal_code) VALUES (107,20236, 'Home', '40 Birchmount Rd', 'Scarborough', 'ON', 'M1T 9M0');
+INSERT INTO Addresses (address_id,customer_id,address_name,street,city,province,postal_code) VALUES (108,20237, 'Home', '54 Oakville Rd', 'Scarborough', 'ON', 'M6G 8H6');
+INSERT INTO Addresses (address_id,customer_id,address_name,street,city,province,postal_code) VALUES (109,20238, 'Office', '560 Queen St', 'Toronto', 'ON', 'M3F 5V6');
+INSERT INTO Addresses (address_id,customer_id,address_name,street,city,province,postal_code) VALUES (110,20239, 'Home', '122 Yonge St', 'Toronto', 'ON', 'M1P 7C0');
+INSERT INTO Addresses (address_id,customer_id,address_name,street,city,province,postal_code) VALUES (111,20240, 'Home', '125 Victoria Park Avenue', 'Toronto', 'ON', 'M1X 4C1');
+INSERT INTO Addresses (address_id,customer_id,address_name,street,city,province,postal_code) VALUES (112,20241, 'Home', '45 Merton St', 'Toronto', 'ON', 'M3P 1B4');
+INSERT INTO Addresses (address_id,customer_id,address_name,street,city,province,postal_code) VALUES (113,20242, 'Home', '123 Bay St', 'Toronto', 'ON', 'M3P 1B5');
+INSERT INTO Addresses (address_id,customer_id,address_name,street,city,province,postal_code) VALUES (114,20243, 'Home', '109 Neilson Avenue', 'Toronto', 'ON', 'M0K 3H5');
+INSERT INTO Addresses (address_id,customer_id,address_name,street,city,province,postal_code) VALUES (115,20244, 'Home', '298 Sheppard Ave', 'Scarborough', 'ON', 'M8U 2T4');
+INSERT INTO Addresses (address_id,customer_id,address_name,street,city,province,postal_code) VALUES (116,20245, 'Office', '131 Markham Rd', 'Scarborough', 'ON ', 'M9B 4A5');
+
+--Customers
+--INSERT INTO Customers (customer_id, customer_name, email) VALUES (20231,'Mack Hensley','mackhensley08@gmail.com');
+--INSERT INTO Customers (customer_id, customer_name, email) VALUES (20232, 'Petra Wilkerson', 'petrawilkerson@yahoo.com');
+--INSERT INTO Customers (customer_id, customer_name, email) VALUES (20233, 'Marcos Flores', 'marcosflores@gmail.com');
+--INSERT INTO Customers (customer_id, customer_name, email) VALUES (20234, 'Rachel Zimmerman', 'rachelzimmerman01@gmail.com');
+--INSERT INTO Customers (customer_id, customer_name, email) VALUES (20235, 'Jonathon Mckenzie', 'jonathonmckenzie@gmail.com');
+--INSERT INTO Customers (customer_id, customer_name, email) VALUES (20236, 'Frederic Oconnor', 'fredericoconnor571@yahoo.com');
+--INSERT INTO Customers (customer_id, customer_name, email) VALUES (20237, 'Norris Kaiser', 'norriskaiser@gmail.com');
+--INSERT INTO Customers (customer_id, customer_name, email) VALUES (20238, 'Rod Buchanan', 'rodbuchanan13@gmail.com');
+--INSERT INTO Customers (customer_id, customer_name, email) VALUES (20239, 'Lonnie Kennedy', 'lonniekennedy01@gmail.com');
+--INSERT INTO Customers (customer_id, customer_name, email) VALUES (20240, 'Catalina Dean', 'catalinadean11@gmail.com');
+--INSERT INTO Customers (customer_id, customer_name, email) VALUES (20241, 'Nellie Burnett', 'nellieburnett@outlook.com');
+--INSERT INTO Customers (customer_id, customer_name, email) VALUES (20242, 'Burl Knight', 'burlknight319@gmail.com');
+--INSERT INTO Customers (customer_id, customer_name, email) VALUES (20243, 'Hester Ayers', 'hesterayers205@outlook.com');
+--INSERT INTO Customers (customer_id, customer_name, email) VALUES (20244, 'Kory Burton', 'koryburton@gmail.com');
+--INSERT INTO Customers (customer_id, customer_name, email) VALUES (20245, 'Rosario Russo', 'rosariorusso@gmail.com');
