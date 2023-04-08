4fab0486ee5eb0bda9c3bee0ef49d1e1da612ccf
Haneef Muhammad <52352973+haneefmhmmd@users.noreply.github.com>
2023-04-08 14:28:59 -0400

    added testing code for procedure UPDATE_CUSTOMER_INFO

--- EXECTION OF PROCEDURES,PACKAGES,FUCTIONS.sql
+++ EXECTION OF PROCEDURES,PACKAGES,FUCTIONS.sql
@@ -17,44 +17,40 @@ BEGIN
 END;
 
 
-
+-- Testing code for the Procedure UPDATE_CUSTOMER_INFO
 DECLARE
-    lv_customer_id CUSTOMERS.CUSTOMER_ID:= 20231;
-    lv_updated_name CUSTOMERS.EMAIL:= 'mackhensley@gmail.com';
+    lv_customer_id CUSTOMERS.CUSTOMER_ID%TYPE:=20231;
     
-    lv_out_customer_id CUSTOMERS.CUSTOMER_ID;
-    lv_out_customer_email CUSTOMERS.EMAIL;
-    
-    lv_order_id ORDERS.ORDER_ID;
-    lv_status ORDER_STATUS.STATUS;
-    
-    lv_updated_order_id ORDERS.ORDER_ID;
-    lv_updated_status ORDER_STATUS.STATUS;
+    lv_out_customer_id CUSTOMERS.CUSTOMER_ID%TYPE;
+    lv_out_customer_email CUSTOMERS.EMAIL%TYPE;
 BEGIN
 
+    SELECT customer_id, email 
+    INTO lv_out_customer_id,lv_out_customer_email
+    FROM customers
+    WHERE customer_id = lv_customer_id;
+
+    DBMS_OUTPUT.PUT_LINE('Customer Email before updation');
+    DBMS_OUTPUT.PUT_LINE('Customer Id: ' ||lv_out_customer_id);
+    DBMS_OUTPUT.PUT_LINE('Customer Email: ' ||lv_out_customer_email);
+
     UPDATE_CUSTOMER_INFO(
-        new_name => lv_customer_id,
-        new_email => lv_updated_name
+        p_customer_id => lv_customer_id,
+        new_email => 'mackhenry@gmail.com'
     );
     
     SELECT customer_id, email 
     INTO lv_out_customer_id,lv_out_customer_email
     FROM customers
     WHERE customer_id = lv_customer_id;
-    
-    DBMS_OUTPUT.PUT_LINE('ID: ' || lv_out_customer_id);
-    DBMS_OUTPUT.PUT_LINE('Updated Email: ' || lv_out_customer_email);    
-    
-    
-    update_order_status(lv_order_id,lv_order_id);
-    SELECT ORDER_ID, STATUS
-    INTO lv_updated_order_id,lv_updated_status
-    FROM ORDERS JOIN ORDER_STATUS USING(STATUS_ID);
-    
-    DBMS_OUTPUT.PUT_LINE('Order Id: ' || lv_updated_order_id);
-    DBMS_OUTPUT.PUT_LINE('Updated Order Status: ' || lv_updated_status);    
+
+    DBMS_OUTPUT.PUT_LINE('');    
+    DBMS_OUTPUT.PUT_LINE('Customer Name after updating email');
+    DBMS_OUTPUT.PUT_LINE('Customer Id: ' ||lv_out_customer_id);
+    DBMS_OUTPUT.PUT_LINE('Updated Customer Email: ' ||lv_out_customer_email);
     
 END;
+
 
 DECLARE
     lv_order_id ORDERS.ORDER_ID%TYPE;
