69e308bb57d5cad76adafd8bef3299b6d3951b54
syed ikram <syed.khaja.ikramuddin9@gmail.com>
2023-04-15 09:48:54 -0400

    adding procedure

--- GroceryOrderingApp/Controllers/HomeController.cs
+++ GroceryOrderingApp/Controllers/HomeController.cs
@@ -1,7 +1,10 @@
 ﻿using GroceryOrderingApp.Data;
 using GroceryOrderingApp.Models;
 using Microsoft.AspNetCore.Mvc;
+using Oracle.ManagedDataAccess.Client;
+using System.Data;
 using System.Diagnostics;
+using System.Security.Cryptography;
 
 namespace GroceryOrderingApp.Controllers
 {
@@ -32,6 +35,40 @@ namespace GroceryOrderingApp.Controllers
         {
             Customer customer = _db.CUSTOMERS.FirstOrDefault(u => u.CUSTOMER_ID == c1.CUSTOMER_ID);
             Console.WriteLine(c1.CUSTOMER_ID);
+            return View(customer);
+        }
+        public IActionResult EditCustomer(Decimal id)
+        {
+            Customer customer = _db.CUSTOMERS.FirstOrDefault(u => u.CUSTOMER_ID == id);
+            return View(customer);
+        }
+        [HttpPost]
+        public IActionResult EditCustomer(Customer c1)
+        {
+            Customer customer = _db.CUSTOMERS.FirstOrDefault(u => u.CUSTOMER_ID == c1.CUSTOMER_ID);
+            //if(Model.IsValid)
+            //{
+
+            //}
+            // Establish a database connection
+            var connectionString = "User Id=COMP214_W23_zo_19;Password=password;Data Source=199.212.26.208:1521/SQLD;";
+            var connection = new OracleConnection(connectionString);
+            connection.Open();
+
+            // Create a command object for the procedure
+            var command = new OracleCommand("update_customer_info", connection);
+            command.CommandType = CommandType.StoredProcedure;
+
+            // Add parameters to the command
+            command.Parameters.Add("p_customer_id", OracleDbType.Int32).Value = c1.CUSTOMER_ID;
+            command.Parameters.Add("new_name", OracleDbType.Varchar2).Value = c1.CUSTOMER_NAME;
+            command.Parameters.Add("new_email", OracleDbType.Varchar2).Value = c1.EMAIL;
+
+            // Execute the command
+            command.ExecuteNonQuery();
+
+            // Close the connection
+            connection.Close();
             return View(customer);
         }
         public IActionResult Privacy()
